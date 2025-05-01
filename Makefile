d6113bd669f6c6291e34770d85597c8564b2f6a4
Juliette Cordor <me@cordor.dev>
2025-05-01 22:52:29 +1000

    updated gitignore

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
 build:
 	gcc main.c -L./target/release -lartcis_bat -ludev
+
+main.o: main.c
+	gcc -c $< -o $@
+
+device.a: device.o
+	ar rcs $@ $^
+
+device.o: device.c
+	gcc -c -o $@ $<
--- device.h
+++ device.h
@@ -16,23 +16,4 @@ struct device_status {
     float battery_level;
 };
 
-int get_device_status(hid_device *handle, struct device_status *status)
-{
-    unsigned char buf[4];
-    int res;
-
-    unsigned char request_buf[2] = { 0x00, 0xb0 };
-    res = hid_write(handle, request_buf, 2);
-
-    // Read requested state
-    res = hid_read(handle, buf, 4);
-
-    if (res < 0) {
-        return -1;
-    }
-
-    status->status = (enum charging_status)buf[3];
-    status->battery_level = ((float)buf[2] / 4.0f) * 100.0f;
-
-    return 0;
-}
+int get_device_status(hid_device *handle, struct device_status *status);
