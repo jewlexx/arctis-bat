373916318c9465d3cd0ad6ec2a5ceefffb25275a
Juliette Cordor <me@cordor.dev>
2025-05-08 10:47:58 +1000

    removed malloc

--- main.c
+++ main.c
@@ -11,22 +11,20 @@
 int main(int argc, char* argv[])
 {
 	int res;
-	unsigned char buf[65];
-	wchar_t wstr[MAX_STR];
-	hid_device *handle;
-	int i;
 
 	// Initialize the hidapi library
 	res = hid_init();
 
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
-	handle = hid_open(VENDOR_ID, PRODUCT_ID, NULL);
+	hid_device *handle = hid_open(VENDOR_ID, PRODUCT_ID, NULL);
 	if (!handle) {
 		printf("Unable to open device\n");
 		hid_exit();
  		return 1;
 	}
+
+	wchar_t wstr[MAX_STR];
 
 	// Read the Manufacturer String
 	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
@@ -36,27 +34,18 @@ int main(int argc, char* argv[])
 	res = hid_get_product_string(handle, wstr, MAX_STR);
 	printf("Product String: %ls\n", wstr);
 
-	// Read the Serial Number String
-	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	printf("Serial Number String: (%d) %ls\n", wstr[0], wstr);
+	struct device_status status = {};
 
-	// Read Indexed String 1
-	// res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	// printf("Indexed String 1: %ls\n", wstr);
-
-	struct device_status *status = malloc(sizeof(struct device_status));
-
-    if (get_device_status(handle, status) < 0) {
+    if (get_device_status(handle, &status) < 0) {
         printf("Failed to get device status\n");
-        free(status);
         hid_close(handle);
         hid_exit();
         return 1;
     }
 
 
-    printf("Charging Status: %d\n", status->status);
-    printf("Battery Level: %.2f%%\n", status->battery_level);
+    printf("Charging Status: %d\n", status.status);
+    printf("Battery Level: %.2f%%\n", status.battery_level);
 
 	// Close the device
 	hid_close(handle);
