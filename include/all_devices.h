c899a25d62ee897b6232f2c247cf795e90f03404
Juliette Cordor <me@cordor.dev>
2025-06-06 16:09:18 +1000

    added headphone count

--- include/all_devices.h
+++ include/all_devices.h
@@ -1,6 +1,6 @@
 #include <stddef.h>
 #include "include/device.h"
 
-const device_identifier known_headphones[];
+const device_identifier known_headphones[20];
 
 const size_t known_headphones_count;
